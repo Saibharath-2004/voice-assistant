9b45c81fa8d32806619f2550368427a816ef315a
Saibharath-2004 <124129618+Saibharath-2004@users.noreply.github.com>
2025-11-26 22:49:44 +0530

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {CarDashboard} from "./components/CarDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <CarDashboard/>
+      
+    </div>
+  );
+}
+
+export default App;
